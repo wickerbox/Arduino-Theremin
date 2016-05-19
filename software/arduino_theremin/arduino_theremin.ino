4744187ee2dbd8697244defa0900f9e8e1429852
Jenner Hanni <jeh.wicker@gmail.com>
2016-05-19 14:35:31 -0700

    Setting the key based on cap sense press.

--- software/arduino_theremin/arduino_theremin.ino
+++ software/arduino_theremin/arduino_theremin.ino
@@ -6,6 +6,9 @@
 //
 // Based on code from Tone example
 // https://www.arduino.cc/en/Tutorial/ToneMelody?from=Tutorial.Tone
+//
+// Using the readCapacitivePin function from Mario Becker, Alan Chatham, and others
+// http://playground.arduino.cc/Code/CapacitiveSensor
 
 #include "pitches.h"
 
@@ -13,6 +16,10 @@
 #define RECORD   1
 #define HOLD     2
 #define PLAYBACK 3
+
+#define A 0
+#define C 1
+#define F 2
 
 #define MAXNOTE  7
 
@@ -31,7 +38,7 @@ int noteArray_F[8] = {NOTE_F1,NOTE_F2,NOTE_F3,NOTE_F4,NOTE_F5,
 // largest note in pitches.h is 4978
 // 2^16 = 65536
 uint16_t current_tune[1024];
-uint16_t curr_key;
+uint16_t curr_key = A;
 uint16_t note_delay; // in milliseconds
 
 // inputs
@@ -53,6 +60,67 @@ long pulse, inches, cm;
 // counter variables
 int count = 0;
 int note = 0;
+
+uint8_t readCapacitivePin(int pinToMeasure) {
+
+  // Variables used to translate from Arduino to AVR pin naming
+  volatile uint8_t* port;
+  volatile uint8_t* ddr;
+  volatile uint8_t* pin;
+  // Here we translate the input pin number from
+  //  Arduino pin number to the AVR PORT, PIN, DDR,
+  //  and which bit of those registers we care about.
+  byte bitmask;
+  port = portOutputRegister(digitalPinToPort(pinToMeasure));
+  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
+  bitmask = digitalPinToBitMask(pinToMeasure);
+  pin = portInputRegister(digitalPinToPort(pinToMeasure));
+  // Discharge the pin first by setting it low and output
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  delay(1);
+  uint8_t SREG_old = SREG; //back up the AVR Status Register
+  // Prevent the timer IRQ from disturbing our measurement
+  noInterrupts();
+  // Make the pin an input with the internal pull-up on
+  *ddr &= ~(bitmask);
+  *port |= bitmask;
+
+  // Now see how long the pin to get pulled up. This manual unrolling of the loop
+  // decreases the number of hardware cycles between each read of the pin,
+  // thus increasing sensitivity.
+  uint8_t cycles = 17;
+  if (*pin & bitmask) { cycles =  0;}
+  else if (*pin & bitmask) { cycles =  1;}
+  else if (*pin & bitmask) { cycles =  2;}
+  else if (*pin & bitmask) { cycles =  3;}
+  else if (*pin & bitmask) { cycles =  4;}
+  else if (*pin & bitmask) { cycles =  5;}
+  else if (*pin & bitmask) { cycles =  6;}
+  else if (*pin & bitmask) { cycles =  7;}
+  else if (*pin & bitmask) { cycles =  8;}
+  else if (*pin & bitmask) { cycles =  9;}
+  else if (*pin & bitmask) { cycles = 10;}
+  else if (*pin & bitmask) { cycles = 11;}
+  else if (*pin & bitmask) { cycles = 12;}
+  else if (*pin & bitmask) { cycles = 13;}
+  else if (*pin & bitmask) { cycles = 14;}
+  else if (*pin & bitmask) { cycles = 15;}
+  else if (*pin & bitmask) { cycles = 16;}
+
+  // End of timing-critical section; turn interrupts back on if they were on before, or leave them off if they were off before
+  SREG = SREG_old;
+
+  // Discharge the pin again by setting it low and output
+  //  It's important to leave the pins low if you want to 
+  //  be able to touch more than 1 sensor at a time - if
+  //  the sensor is left pulled high, when you touch
+  //  two sensors, your body will transfer the charge between
+  //  sensors.
+  *port &= ~(bitmask);
+  *ddr  |= bitmask;
+  return cycles;
+}
 
 void setup() {
 
@@ -92,20 +160,25 @@ void loop() {
     case RECORD:
       digitalWrite(recordLEDPin, HIGH);
       digitalWrite(playbackLEDPin, LOW);
-
+      
       for (count = 0; count < MAXNOTE; count++) {
+          tone(3,noteArray_A[count],1000/8);
           Serial.print(noteArray_A[count]);
           Serial.print(" ");
+          delay(800);
       }
       Serial.println(" ");
       for (count = 0; count < MAXNOTE; count++) {
+          tone(3,noteArray_C[count],1000/8);
           Serial.print(noteArray_C[count]);
           Serial.print(" ");
-      }
+          delay(800);     }
       Serial.println(" ");
       for (count = 0; count < MAXNOTE; count++) {
+          tone(3,noteArray_F[count],1000/8);
           Serial.print(noteArray_F[count]);
           Serial.print(" ");
+           delay(800);
       }
       Serial.println(" ");
       
@@ -117,6 +190,7 @@ void loop() {
         note = 7;
       note = note - 7;
       Serial.println(note);
+
       delay(1000);
       
       break;
@@ -127,6 +201,28 @@ void loop() {
     case PLAYBACK:
       digitalWrite(recordLEDPin, LOW);
       digitalWrite(playbackLEDPin, HIGH);
+
+      int a = readCapacitivePin(aKeyPin);
+      int c = readCapacitivePin(cKeyPin);
+      int f = readCapacitivePin(fKeyPin);
+
+      Serial.print(a);
+      Serial.print(" ");
+      Serial.print(c);
+      Serial.print(" ");
+      Serial.println(f);
+
+      if (a > 5)
+        curr_key = A;
+      else if (c > 5)
+        curr_key = C;
+      else if (f > 5)
+        curr_key = F;
+      else
+        curr_key = curr_key;
+
+      Serial.println(curr_key);
+      delay(200);
       break;
   }
 
